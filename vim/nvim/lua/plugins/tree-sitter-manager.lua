9dc30075d45043927edb4333403b73742a62f509
Michael Schmitz <michael.schmitz@swissuccess.ch>
2026-05-30 13:56:22 +0200

    Migrate away from archived 'nvim-treesitter' to tsmanager plugin

---
+++ vim/nvim/lua/plugins/tree-sitter-manager.lua
@@ -0,0 +1,28 @@
+return {
+	"romus204/tree-sitter-manager.nvim",
+	dependencies = {}, -- tree-sitter CLI must be installed system-wide
+	config = function()
+		require("tree-sitter-manager").setup({
+			-- Default Options
+			ensure_installed = {
+				"lua",
+				"html",
+				"html_tags",
+				"css",
+				"javascript",
+				"typescript",
+				"angular",
+				"c_sharp",
+				"json",
+				"java",
+				"javadoc",
+				"markdown",
+				"bash",
+			}, -- list of parsers to install at the start of a neovim session. If set to "all", install all parsers.
+			-- border = nil, -- border style for the window (e.g. "rounded", "single"), if nil, use the default border style defined by 'vim.o.winborder'. See :h 'winborder' for more info.
+			-- auto_install = false, -- if enabled, install missing parsers when editing a new file
+			-- highlight = true, -- treesitter highlighting is enabled by default
+			-- languages = {}, -- override or add new parser sources
+		})
+	end,
+}
